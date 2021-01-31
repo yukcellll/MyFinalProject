4afb6b60ec728b28c8a1e0f83cca5d1a66992f8d
Yüksel ERDOĞAN <yukcellll@hotmail.com>
2021-01-31 13:11:22 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitInStock { get; set; }
+        public int UnitPrice { get; set; }
+
+    }
+}
